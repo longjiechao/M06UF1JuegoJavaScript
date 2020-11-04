59ef0aa50d34dff115608754f2d8077d572caac6
Longjie Chao <chao.long.jie@alumnat.copernic.cat>
2020-11-04 18:00:10 +0100

    lj cambios index para que se abra los 3 a la vez

--- public_html/data.js
+++ public_html/data.js
@@ -3,5 +3,50 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var palabra = ["p1", "p2", "p3", "p4", "p5"];
 
-var palabra = "holaaaa Que tal estas' ".toUpperCase();  
+setCookie("p1", "Zorro");
+setCookie("p2", "Tribunal");
+setCookie("p3", "Cachorro");
+setCookie("p4", "Letrado");
+setCookie("p5", "Encadenado");
+
+
+
+function getRandom(){
+    return Math.random() * (5 - 0) + 0 ;
+}
+
+function setCookie(cname, cvalue) {
+  var d = new Date();
+  d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+} 
